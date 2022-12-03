34d979f80cd3de0b81d50c456707091d279b622d
steve2811 <dns2811@gmail.com>
2022-12-03 13:15:14 +0100

    auto-rename completed

--- XWB-Extractor/XWB-Extractor.ps1
+++ XWB-Extractor/XWB-Extractor.ps1
@@ -1,47 +1,33 @@
 ﻿
 # Change directory: place you "towav"-wav folder as next variable
-### GAB ###
-$inputFolder = "C:\MISE-ITA\DoppiaggioITA\traccewav" #input
-### STEVE ###
-# $toWavExtactionPath = "D:\Progetti Personali\Monkey Island - Doppiaggio in italiano\audio estratto\MISE-ITA\DoppiaggioITA\audio estratto\con numero davanti (towav)"
+$ToWavOutputFolder = "C:\MISE-ITA\MISE-ITA-Master\Dialoghi\Tracce-WAV"
 
-$renamedFolder = "C:\MISE-ITA\MISE-ITA-Master\Dialoghi\Tracce-WAV" #output
-# Set-Location $toWavExtactionPath # cd path
 
 <#
 Sezione dedicata a towav.exe per estrazione wav da XWB file originale
 #>
 
 
-$fileList = Get-ChildItem $inputFolder -Filter *.wav
-$fileName = [string[]]::new($fileList.Length) # initiate the array
+$fileList = Get-ChildItem $ToWavOutputFolder -Filter *.wav
 ForEach ($thisFile in $fileList.Name) {
     $number = $thisFile.Split(" ")[0]
-    #$newFileName = $number
-    exit
-}
-
-<#
-foreach file presente nella cartella {
-    if nome del file soddisfa "(\d+)+\s(.*.wav)"{ //subpattern 1 = numero iniziale, subpattern 2 = nome file senza numero iniziale
-       if subpattern1 ha una cifra
-          rinomina in ("0000" + subpattern1 + "_" + subpattern2)
-       if subpattern1 ha 2 cifre...
-       if subpattern1 ha 3 cifre...
-       if subpattern1 ha 4 cifre...
- }
-#>
-
-<#
-Get-ChildItem . -Filter *.wav | ForEach-Object {
-    $CurrentFile = $_
-    ForEach ($wavNewName in $wavNewNameList) {
-        if ($CurrentFile.Name -ilike $wavNewName.originalName) {
-            Rename-Item $CurrentFile.Name $wavNewName.finalName
-            Write-Host "Renaming " $CurrentFile " with " $wavNewName "..."
-            Write-Host "File renamed!"
-            Write-Host "##########"
+    $wavName = $thisFile.Split(" ")[1] # Assumption: towav.exe output file name is as "<number> filename.wav"
+    
+    if ($thisFile.Split(" ").Length -gt 1) { #If there are no spaces in file name (file name already renamed)
+        if ($number.ToString().Length -eq 1) {
+            Rename-Item -Path $ToWavOutputFolder"\"$thisFile -NewName ("0000" + $number + "_" + $wavName)
+        }
+        elseif ($number.ToString().Length -eq 2) {
+            Rename-Item -Path $ToWavOutputFolder"\"$thisFile -NewName ("000" + $number + "_" + $wavName)
+        }
+        elseif ($number.ToString().Length -eq 3) {
+            Rename-Item -Path $ToWavOutputFolder"\"$thisFile -NewName ("00" + $number + "_" + $wavName)
+        }
+        elseif ($number.ToString().Length -eq 4) {
+            Rename-Item -Path $ToWavOutputFolder"\"$thisFile -NewName ("0" + $number + "_" + $wavName)
+        }
+        else {
+            Rename-Item -Path $ToWavOutputFolder"\"$thisFile -NewName ($number + "_" + $wavName)
         }
     }
 }
-#>
