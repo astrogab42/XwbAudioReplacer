7e9fef601613d8c2e6c816a09a6a0e95daf1e974
steve2811 <dns2811@gmail.com>
2022-12-03 11:31:16 +0100

    Estrazione numero dal nome file

--- XWB-Extractor/XWB-Extractor.ps1
+++ XWB-Extractor/XWB-Extractor.ps1
@@ -1,20 +1,22 @@
 ﻿
-
 # Change directory: place you "towav"-wav folder as next variable
-$toWavExtactionPath = "C:\MISE-ITA\DoppiaggioITA\audio estratto\con numero davanti (towav)"
+# $toWavExtactionPath = "C:\MISE-ITA\DoppiaggioITA\audio estratto\con numero davanti (towav)"
+$toWavExtactionPath = "D:\Progetti Personali\Monkey Island - Doppiaggio in italiano\audio estratto\MISE-ITA\DoppiaggioITA\audio estratto\con numero davanti (towav)"
 Set-Location $toWavExtactionPath # cd path
 
-# WAV file list with new names
-$wavNewNameList = Import-Csv "C:\MISE-ITA\DoppiaggioITA\audio estratto\ListaWavNames.csv"
-
 Get-ChildItem . -Filter *.wav | ForEach-Object {
-    $CurrentFile = $_
-    ForEach ($wavNewName in $wavNewNameList) {
-        if ($CurrentFile.Name -ilike $wavNewName.originalName) {
-            Rename-Item $CurrentFile.Name $wavNewName.finalName
-            Write-Host "Renaming " $CurrentFile " with " $wavNewName "..."
-            Write-Host "File renamed!"
-            Write-Host "##########"
-        }
-    }
+    $number = $_.Name.Split(" ")[0]
+    Write-Host $number
+    exit
 }
+
+<#
+foreach file presente nella cartella {
+    if nome del file soddisfa "(\d+)+\s(.*.wav)"{ //subpattern 1 = numero iniziale, subpattern 2 = nome file senza numero iniziale
+       if subpattern1 ha una cifra
+          rinomina in ("0000" + subpattern1 + "_" + subpattern2)
+       if subpattern1 ha 2 cifre...
+       if subpattern1 ha 3 cifre...
+       if subpattern1 ha 4 cifre...
+ }
+#>
