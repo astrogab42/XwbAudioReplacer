60d506ff5ddb24868d91ec1582ee3862f434dde8
astrogab <gabriele.csl@gmail.com>
2022-12-03 13:25:07 +0100

    Minor netiquette

--- XWB-Extractor/XWB-Extractor.ps1
+++ XWB-Extractor/XWB-Extractor.ps1
@@ -1,32 +1,32 @@
 ﻿##### INITIALIZATION #####
-$ToWavOutputFolder = "C:\MISE-ITA\MISE-ITA-Master\Dialoghi\Tracce-WAV"
-$SpeechxwbOriginal = "C:\MISE-ITA\MISE-ITA-Master\originalSpeechFiles\Speech.xwb"
+$wavOutputFolder = "C:\MISE-ITA\MISE-ITA-Master\Dialoghi\Tracce-WAV"
+$xwbInputFile = "C:\MISE-ITA\MISE-ITA-Master\originalSpeechFiles\Speech.xwb"
 
 # extract wav from xwb
-$extractWav = .\towav.exe $SpeechxwbOriginal 
-Move-Item *.wav $ToWavOutputFolder
+$extractWav = .\towav.exe $xwbInputFile 
+Move-Item *.wav $wavOutputFolder
 
 # Rename files
-$fileList = Get-ChildItem $ToWavOutputFolder -Filter *.wav
+$fileList = Get-ChildItem $wavOutputFolder -Filter *.wav
 ForEach ($thisFile in $fileList.Name) {
     $number = $thisFile.Split(" ")[0]
     $wavName = $thisFile.Split(" ")[1] # Assumption: towav.exe output file name is as "<number> filename.wav"
     
     if ($thisFile.Split(" ").Length -gt 1) { #If there are no spaces in file name (file name already renamed)
         if ($number.ToString().Length -eq 1) {
-            Rename-Item -Path $ToWavOutputFolder"\"$thisFile -NewName ("0000" + $number + "_" + $wavName)
+            Rename-Item -Path $wavOutputFolder"\"$thisFile -NewName ("0000" + $number + "_" + $wavName)
         }
         elseif ($number.ToString().Length -eq 2) {
-            Rename-Item -Path $ToWavOutputFolder"\"$thisFile -NewName ("000" + $number + "_" + $wavName)
+            Rename-Item -Path $wavOutputFolder"\"$thisFile -NewName ("000" + $number + "_" + $wavName)
         }
         elseif ($number.ToString().Length -eq 3) {
-            Rename-Item -Path $ToWavOutputFolder"\"$thisFile -NewName ("00" + $number + "_" + $wavName)
+            Rename-Item -Path $wavOutputFolder"\"$thisFile -NewName ("00" + $number + "_" + $wavName)
         }
         elseif ($number.ToString().Length -eq 4) {
-            Rename-Item -Path $ToWavOutputFolder"\"$thisFile -NewName ("0" + $number + "_" + $wavName)
+            Rename-Item -Path $wavOutputFolder"\"$thisFile -NewName ("0" + $number + "_" + $wavName)
         }
         else {
-            Rename-Item -Path $ToWavOutputFolder"\"$thisFile -NewName ($number + "_" + $wavName)
+            Rename-Item -Path $wavOutputFolder"\"$thisFile -NewName ($number + "_" + $wavName)
         }
     }
 }
