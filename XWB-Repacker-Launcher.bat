2f998ef20792044a59c9392493af33ecb2b172df
astrogab <gabriele.csl@gmail.com>
2024-05-08 17:30:41 +0200

    Fixed Repacker Launcher with pwsh.exe

--- XWB-Repacker-Launcher.bat
+++ XWB-Repacker-Launcher.bat
@@ -1,5 +1,4 @@
 @echo off
-set scriptName=XWB-Repacker.ps1
-
-for /f "delims=" %%i in ('PowerShell -Command "Get-ChildItem -Path '.\' -Filter '%scriptName%' -Recurse | Select-Object -ExpandProperty FullName | Resolve-Path -Relative"') do set scriptPath=%%i
-PowerShell.exe -noexit -NoProfile -ExecutionPolicy Bypass -Command "& '%scriptPath%'"
+set scriptName=.\XWB-Repacker.ps1
+cd XwbAudioReplacer
+pwsh.exe -noexit -NoProfile -ExecutionPolicy Bypass -Command "& '%scriptName%'"
